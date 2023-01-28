519277a1eecbae7496c2c6e36ecf7e8be9058710
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-01-27 20:24:39 -0600

    Initial commit

---
+++ lib/pages/mapa_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MapaPage extends StatelessWidget {
+   
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+         child: Text('MapaPage'),
+      ),
+    );
+  }
+}
