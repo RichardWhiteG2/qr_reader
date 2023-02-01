63e5f8736764d58eeec97d13f7e918142c8842f3
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-01-31 21:01:02 -0600

    Leer Scans de la BD con Provider

---
+++ lib/providers/scan_list_provider.dart
@@ -0,0 +1,49 @@
+
+import 'package:flutter/material.dart';
+import 'package:qr_reader/providers/db_provider.dart';
+
+//Para actualizar la UI de bd.
+
+class ScanListProvider extends ChangeNotifier {
+
+  List<ScanModel> scans = [];
+  String tipoSeleccionado = 'http';
+
+  //Se inserta un registro pero noa ctualiza ui
+  nuevoScan(String valor) async{
+
+    final nuevoScan = new ScanModel(valor: valor);
+    final id = await DBProvider.db.nuevoScan(nuevoScan);
+
+    nuevoScan.id = id;
+    if(this.tipoSeleccionado == nuevoScan.tipo){
+      this.scans.add(nuevoScan);
+      notifyListeners();
+    }
+  }
+
+  cargarScans() async{
+    final scans = await DBProvider.db.getTodosLosScans();
+    this.scans = [...scans];
+    notifyListeners();
+  }
+
+  cargarScansPorTipo(String tipo) async{
+    final scans = await DBProvider.db.getScansPorTipo(tipo);
+    this.scans = [...scans];
+    this.tipoSeleccionado = tipo;
+    notifyListeners();
+  }
+  borrarTodos() async {
+    await DBProvider.db.deleteAllScan();
+    this.scans = [];
+    notifyListeners();
+  }
+  borrarScanPorId ( int id ) async {
+    await DBProvider.db.deleteScan(id);
+    this.cargarScansPorTipo(this.tipoSeleccionado);
+    
+  }
+
+
+}
