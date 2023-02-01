3bfbe3f872a9da38b86989b42c8efc6416ead7a0
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-01-31 21:10:36 -0600

    Mostrar mapas UI

--- lib/pages/mapas_pages.dart
+++ lib/pages/mapas_pages.dart
@@ -6,6 +6,15 @@ class MapasPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Mapas Page'),);
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (_ , i) => ListTile(
+        leading: Icon(Icons.map, color: Theme.of(context).primaryColor ) ,
+        title: Text('http://asdfghkjl.com'),
+        subtitle: Text('ID: 1'),
+        trailing: Icon(Icons.keyboard_arrow_right, color:  Colors.grey,),
+        onTap: () =>  print('Abrir algo.'),
+      )
+    );
   }
 }
